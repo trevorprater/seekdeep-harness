90aaf03401e1d89991f7843e37e0dfd727d07288
trevorprater <trevor.prater@gmail.com>
2026-08-26 06:39:58 -0400

    feat: port client icon catalog to Rust WASM

--- crates/client-ui-primitives/src/lib.rs
+++ crates/client-ui-primitives/src/lib.rs
@@ -4,14 +4,21 @@ mod ansi;
 #[cfg(target_arch = "wasm32")]
 mod browser_atoms;
 #[cfg(target_arch = "wasm32")]
+mod browser_icons;
+#[cfg(target_arch = "wasm32")]
 mod browser_util;
 mod head_tail_cap;
+#[allow(clippy::needless_raw_string_hashes, clippy::unreadable_literal)]
+mod icon_data;
 mod markdown;
 
 pub use ansi::*;
 #[cfg(target_arch = "wasm32")]
 pub use browser_atoms::*;
 #[cfg(target_arch = "wasm32")]
+pub use browser_icons::*;
+#[cfg(target_arch = "wasm32")]
 pub use browser_util::*;
 pub use head_tail_cap::*;
+pub use icon_data::*;
 pub use markdown::*;
