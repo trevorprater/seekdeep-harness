08118ebe92e7086022848afa26d204140a4e62dd
trevorprater <trevor.prater@gmail.com>
2026-08-29 00:46:05 -0400

    feat: port context meter to Rust WASM

--- crates/client-ui-conversation/src/lib.rs
+++ crates/client-ui-conversation/src/lib.rs
@@ -15,6 +15,8 @@ mod browser_chat_seat;
 #[cfg(target_arch = "wasm32")]
 mod browser_command;
 #[cfg(target_arch = "wasm32")]
+mod browser_context_meter;
+#[cfg(target_arch = "wasm32")]
 mod browser_details_panel;
 #[cfg(target_arch = "wasm32")]
 mod browser_enter_behavior;
@@ -46,6 +48,8 @@ pub use browser_assistant::*;
 pub use browser_chat_seat::*;
 #[cfg(target_arch = "wasm32")]
 pub use browser_command::*;
+#[cfg(target_arch = "wasm32")]
+pub use browser_context_meter::*;
 #[cfg(target_arch = "wasm32")]
 pub use browser_details_panel::*;
 #[cfg(target_arch = "wasm32")]
